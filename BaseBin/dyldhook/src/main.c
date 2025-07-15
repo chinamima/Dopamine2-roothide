13e225452d6edbbf297585962b877416c861dd65
guojinjun <guojinjun@bytedance.com>
2025-07-15 23:02:16 +0800

    log3

--- BaseBin/dyldhook/src/main.c
+++ BaseBin/dyldhook/src/main.c
@@ -8,8 +8,10 @@
 
 #include "dyld.h"
 #include "dyld_jbinfo.h"
-#include <syslog.h>
-#include <os/log.h>
+
+// #include <syslog.h>
+// #include <os/log.h>
+#include <stdio.h>
 
 __attribute__((section("__DATA,__jbinfo"))) static char jbinfoSection[0x4000];
 #define jbInfo ((struct dyld_jbinfo *)&jbinfoSection[0])
@@ -65,9 +67,9 @@ void dyldhook_perform_checkin(void)
 void dyldhook_init(uintptr_t kernelParams)
 {
 	// JBLogDebug("========= dyldhook_init enter");
-    syslog(LOG_ERR, "========= dyldhook_init enter, syslog");
+    // syslog(LOG_ERR, "========= dyldhook_init enter, syslog");
 	// os_log(OS_LOG_DEFAULT, "========= dyldhook_init enter, os_log");
-	printf("========= dyldhook_init enter, printf");
+	printf("========= dyldhook_init enter, printf\n");
 
 	extern void dyldhook_init_roothide(uintptr_t);
 	dyldhook_init_roothide(kernelParams);
